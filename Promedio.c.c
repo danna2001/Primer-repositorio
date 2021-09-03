68bec4c71fabfe42efaba8e490e9eff0f457bd74
Danna Castiblanco <danna.castiblanco@uptc.edu.co>
2021-09-02 20:54:40 -0500

    Agregamos funciones

--- Promedio.c.c
+++ Promedio.c.c
@@ -1,86 +1,62 @@
 #include <stdio.h>
 
+
+
+void showMessageWrongValueAndExit (){
+printf("valor incorrecto \n");
+    exit(0);
+}
+void correctValue (float score ){
+    if (score <0 || score >5){
+        showMessageWrongValueAndExit();
+    }
+}
+
+float requestScore (char* scoreNumber){
+    float score;
+    printf("Ingresar calificacion: ", scoreNumber );
+    scanf("%f", &score);
+    correctValue(score);
+return score;
+}
+float calcProm(
+
+    float N1,
+    float N2,
+    float N3,
+    float N4,
+    float N5
+    
+){
+return (N1 + N2 + N3 + N4 + N5)/ 5;
+}
+
+void correctProm (float Prom){
+    printf("Su promedio es: %f \n", Prom);
+if (Prom >= 2.95){
+    printf("Aprobado");
+}else{   
+    printf ("Reprobado");}
+}
+   
+   
 float main ()
 {
     printf("Bienvenido, esta aplicacion le ayuda a calcular el promedio de cinco notas \n" );
 
-
-    float N1;
-    float N2;
-    float N3;
-    float N4;
-    float N5;
-    float promedio;
-
-   printf("Ingrese la primera nota \n");
-   scanf("%f" , &N1);
-
-if (N1 > 5){
-    printf("valor incorrecto \n");
-    exit(0);
-}
-if (N1 <0){
-    printf("valor incorrecto \n");
-    exit(0);
-}
+   float N1, N2, N3, N4, N5;
+   float Prom;
 
 
-   printf("Ingrese la segunda nota \n");
-   scanf("%f" , &N2);
-   
-if (N2 > 5){
-    printf("valor incorrecto \n");
-    exit(0);
-}
-if (N2 <0){
-    printf("valor incorrecto \n");
-    exit(0);
-}
+    N1= requestScore("primera");
+    N2= requestScore("segunda");
+    N3= requestScore("tercera");
+    N4= requestScore("cuarta");
+    N5= requestScore("quinta");
 
+    Prom = calcProm(N1,N2,N3,N4,N5);
 
-   printf("Ingrese la tercera nota \n");
-    scanf("%f" , &N3);
-if (N3 > 5){
-    printf("valor incorrecto \n");
-    exit(0);
-}
-if (N3 <0){
-    printf("valor incorrecto \n");
-    exit(0);
-}
-
-
-   printf("Ingrese la cuarta nota \n");
-   scanf("%f" , &N4);
-if (N4 > 5){
-    printf("valor incorrecto \n");
-    exit(0);
-}
-if (N4 <0){
-    printf("valor incorrecto \n");
-    exit(0);
-}
-
-
-   printf("Ingrese la quinta nota \n");
-   scanf("%f" , &N5);
-if (N5 > 5){
-    printf("valor incorrecto \n");
-    exit(0);
-}
-if (N5 <0){
-    printf("valor incorrecto \n");
-    exit(0);
-}
-
-
-   promedio= (N1+N2+N3+N4+N5)/ 5;
-   printf("Su promedio es: %f \n", promedio);
-if (promedio >= 2.95){
-    printf("Aprobado");
-}else{   
-    printf ("Reprobado");
-}
+    correctProm(Prom);
 
  return 0;
 
